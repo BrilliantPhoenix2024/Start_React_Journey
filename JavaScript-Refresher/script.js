8ff7f580df3dc9df87db1a1aa190ec6004992e57
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-03-05 17:13:21 +0330

    Understanding _let_ and _const variables

---
+++ JavaScript-Refresher/script.js
@@ -0,0 +1,17 @@
+// Intro to VAR variable
+// var myName = 'Atefeh';
+// console.log(myName);
+// myName = 'Barf';
+// console.log(myName);
+
+// Intro to LET variable
+// let myName = 'Atefeh';
+// console.log(myName);
+// myName = 'Barf';
+// console.log(myName);
+
+// Intro to CONST variable
+const myName = 'Atefeh';
+console.log(myName);
+myName = 'Barf';
+console.log(myName);
