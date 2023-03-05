394ba37db028aa8bcee342115dd38bc9fe6667da
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-03-05 17:39:21 +0330

    Arrow Functions

--- JavaScript-Refresher/script.js
+++ JavaScript-Refresher/script.js
@@ -1,17 +1,49 @@
+// Understanding _let_ and _const variables
+
+/////////////////////////////
 // Intro to VAR variable
 // var myName = 'Atefeh';
 // console.log(myName);
 // myName = 'Barf';
 // console.log(myName);
 
+///////////////////////////////
 // Intro to LET variable
 // let myName = 'Atefeh';
 // console.log(myName);
 // myName = 'Barf';
 // console.log(myName);
 
+/////////////////////////////////
 // Intro to CONST variable
 const myName = 'Atefeh';
 console.log(myName);
-myName = 'Barf';
-console.log(myName);
+// myName = 'Barf';
+// console.log(myName);
+
+////////////////////////////////////
+// Arrow Functions
+function printMyName(name) {
+  console.log(name);
+}
+
+// printMyName();
+printMyName('Atefeh');
+
+const printMyName1 = () => {
+  console.log('Atefeh');
+};
+
+printMyName1();
+
+const printMyName2 = (name1, age) => {
+  console.log(name1, age);
+};
+
+printMyName1('Atefeh', 32);
+
+// const multiply = number => {
+//   return number * 2;
+// };
+const multiply = number => number * 2;
+console.log(multiply(2));
