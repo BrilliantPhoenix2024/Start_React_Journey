58720b8088e9b772ba5cc93464edcd1b0bd28a38
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-03-08 14:00:48 +0330

    Writing More Complex JSX Code

--- React-Basics-Working-With-Components/react-complete-guide/src/components/ExpenseItem.js
+++ React-Basics-Working-With-Components/react-complete-guide/src/components/ExpenseItem.js
@@ -1,7 +1,15 @@
 // AComponents in React is just a JavaScript Function.
 
 function ExpenseItem() {
-  return <h2>Expense item!</h2>;
+  return (
+    <div>
+      <div>March 8th 2032</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
