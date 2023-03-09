f905c27c144d3f697758485e20dd6c0c956fe36a
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-03-09 15:58:05 +0330

    Passing Data via _props

--- React-Basics-Working-With-Components/react-complete-guide/src/App.js
+++ React-Basics-Working-With-Components/react-complete-guide/src/App.js
@@ -3,25 +3,34 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    { title: 'Car Insurance', amount: 297.69, date: new Date(2028, 2, 9) },
+    { title: 'Toilet Paper', amount: 94.12, date: new Date(2023, 2, 9) },
+    { title: 'New Desk', amount: 70.69, date: new Date(2023, 9, 9) },
+    { title: 'New Laptop', amount: 197.69, date: new Date(2023, 12, 9) },
+  ];
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>let's get started!</p>
-      </header> */}
-      {/* This is a Custom made Components (an HTML element) with just starting with uppercase Character */}
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
--- React-Basics-Working-With-Components/react-complete-guide/src/components/ExpenseItem.js
+++ React-Basics-Working-With-Components/react-complete-guide/src/components/ExpenseItem.js
@@ -1,17 +1,13 @@
 import './ExpenseItem.css';
 
 // A Components in React is just a JavaScript Function.
-function ExpenseItem() {
-  const expenseDate = new Date(2032, 2, 9);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 297.69;
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   );
