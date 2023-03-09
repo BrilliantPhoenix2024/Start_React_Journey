2e6351b579adc28b8a7954c17fa58847e7c84823
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-03-09 23:05:07 +0330

    A Closer Look At JSX

--- React-Basics-Working-With-Components/react-complete-guide/src/App.js
+++ React-Basics-Working-With-Components/react-complete-guide/src/App.js
@@ -1,6 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
 import Expenses from './components/Expenses';
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
   const expenses = [
@@ -9,11 +11,18 @@ function App() {
     { title: 'New Desk', amount: 70.69, date: new Date(2023, 9, 9) },
     { title: 'New Laptop', amount: 197.69, date: new Date(2023, 12, 9) },
   ];
-  return (
-    <div>
-      <Expenses items={expenses}></Expenses>
-    </div>
+
+  return React.createElement(
+    'div',
+    {},
+    React.createElement(Expenses, { items: expenses })
   );
+
+  // return (
+  //   <div>
+  //     <Expenses items={expenses}></Expenses>
+  //   </div>
+  // );
 }
 
 export default App;
