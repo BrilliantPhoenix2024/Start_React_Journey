4c94402ef5604490cb5a37e82c240a59b2789840
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-03-09 13:49:38 +0330

    Outputting Dynamic Data & Working with Expressions in JSX

--- React-Basics-Working-With-Components/react-complete-guide/src/components/ExpenseItem.js
+++ React-Basics-Working-With-Components/react-complete-guide/src/components/ExpenseItem.js
@@ -2,12 +2,16 @@ import './ExpenseItem.css';
 
 // A Components in React is just a JavaScript Function.
 function ExpenseItem() {
+  const expenseDate = new Date(2032, 2, 9);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 297.69;
+
   return (
     <div className="expense-item">
-      <div>March 8th 2032</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmount}</div>
       </div>
     </div>
   );
