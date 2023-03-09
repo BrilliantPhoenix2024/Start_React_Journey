4d5ba5de28d6601261586e13101b7906a2d79530
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-03-09 22:38:44 +0330

    The Concept of _Composition_ (_children props_)

--- React-Basics-Working-With-Components/react-complete-guide/src/App.js
+++ React-Basics-Working-With-Components/react-complete-guide/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 function App() {
   const expenses = [
@@ -10,27 +10,8 @@ function App() {
     { title: 'New Laptop', amount: 197.69, date: new Date(2023, 12, 9) },
   ];
   return (
-    <div className="App">
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+    <div>
+      <Expenses items={expenses}></Expenses>
     </div>
   );
 }
--- React-Basics-Working-With-Components/react-complete-guide/src/components/ExpenseItem.js
+++ React-Basics-Working-With-Components/react-complete-guide/src/components/ExpenseItem.js
@@ -1,17 +1,18 @@
 import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 import './ExpenseItem.css';
 
 // A Components in React is just a JavaScript Function.
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       {/* <ExpenseDate /> */}
       <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className="expense-item__price">{props.amount}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
