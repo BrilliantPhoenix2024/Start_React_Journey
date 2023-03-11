4e995b28cd695ae671d8914623855752dc99ba44
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-03-12 00:08:04 +0330

    Listening to Events & Working with Event Handlers

---
+++ React-State-Working-with-Events/react-complete-guide/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import Expenses from './components/Expenses/Expenses';
+// import logo from './logo.svg';
+// import './App.css';
+
+const App = () => {
+  const expenses = [
+    { title: 'Car Insurance', amount: 297.69, date: new Date(2028, 2, 9) },
+    { title: 'Toilet Paper', amount: 94.12, date: new Date(2023, 2, 9) },
+    { title: 'New Desk', amount: 70.69, date: new Date(2023, 9, 9) },
+    { title: 'New Laptop', amount: 197.69, date: new Date(2023, 12, 9) },
+  ];
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
+  return (
+    <div>
+      <Expenses items={expenses}></Expenses>
+    </div>
+  );
+};
+
+export default App;
