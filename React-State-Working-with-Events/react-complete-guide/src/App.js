cea9fd1803ae2e45972e5cb96e9c6e8776f6ee3d
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-03-22 20:10:30 +0330

    Understanding _Keys

--- React-State-Working-with-Events/react-complete-guide/src/App.js
+++ React-State-Working-with-Events/react-complete-guide/src/App.js
@@ -4,17 +4,32 @@ import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
 const DUMMY_EXPENSES = [
-  { title: 'Car Insurance', amount: 297.69, date: new Date(2028, 2, 9) },
-  { title: 'Toilet Paper', amount: 94.12, date: new Date(2023, 2, 9) },
-  { title: 'New Desk', amount: 70.69, date: new Date(2023, 9, 9) },
-  { title: 'New Laptop', amount: 197.69, date: new Date(2023, 12, 9) },
+  {
+    id: 'e1',
+    title: 'Car Insurance',
+    amount: 297.69,
+    date: new Date(2028, 2, 9),
+  },
+  {
+    id: 'e2',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2023, 2, 9),
+  },
+  { id: 'e3', title: 'New Desk', amount: 70.69, date: new Date(2023, 9, 9) },
+  {
+    id: 'e4',
+    title: 'New Laptop',
+    amount: 197.69,
+    date: new Date(2023, 12, 9),
+  },
 ];
 
 const App = () => {
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = expense => {
-    setExpenses((prevExpenses) => {
+    setExpenses(prevExpenses => {
       return [expense, ...prevExpenses];
     });
   };
