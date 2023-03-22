8d7e609f66e2541978f847a45ac29e0ee5194db9
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-03-22 19:48:19 +0330

    Using Stateful Lists

--- React-State-Working-with-Events/react-complete-guide/src/App.js
+++ React-State-Working-with-Events/react-complete-guide/src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
+const DUMMY_EXPENSES = [
+  { title: 'Car Insurance', amount: 297.69, date: new Date(2028, 2, 9) },
+  { title: 'Toilet Paper', amount: 94.12, date: new Date(2023, 2, 9) },
+  { title: 'New Desk', amount: 70.69, date: new Date(2023, 9, 9) },
+  { title: 'New Laptop', amount: 197.69, date: new Date(2023, 12, 9) },
+];
+
 const App = () => {
-  const expenses = [
-    { title: 'Car Insurance', amount: 297.69, date: new Date(2028, 2, 9) },
-    { title: 'Toilet Paper', amount: 94.12, date: new Date(2023, 2, 9) },
-    { title: 'New Desk', amount: 70.69, date: new Date(2023, 9, 9) },
-    { title: 'New Laptop', amount: 197.69, date: new Date(2023, 12, 9) },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
