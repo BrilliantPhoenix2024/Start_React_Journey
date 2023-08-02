0f40fde4fd938f134dc18409e5aaeda5651a2f3c
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-08-03 01:04:31 +0330

    Working with Links

--- Building-Multi-Page-SPA-with-React-Router/src/App.js
+++ Building-Multi-Page-SPA-with-React-Router/src/App.js
@@ -2,21 +2,22 @@ import { Route } from "react-router-dom";
 
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
+import MainHeader from "./componenets/MainHeader";
 
 function App() {
   return (
     <div>
-      <Route path="/welcome">
-        <Welcome />
-      </Route>
-      <Route path="/products">
-        <Products />
-      </Route>
+      <MainHeader />
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+      </main>
     </div>
   );
 }
 
 export default App;
-
-// our-domain.com/welcome => Welcome Component
-// our-domain.com/products => Products Component
