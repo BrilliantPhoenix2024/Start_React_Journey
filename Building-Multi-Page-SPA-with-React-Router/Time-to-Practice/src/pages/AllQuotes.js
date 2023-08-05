82b3c0fd90156500598a157d3d8a48d6d231cfad
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-08-05 16:56:43 +0330

    Adding Dummy Data & More Content

--- Building-Multi-Page-SPA-with-React-Router/Time-to-Practice/src/pages/AllQuotes.js
+++ Building-Multi-Page-SPA-with-React-Router/Time-to-Practice/src/pages/AllQuotes.js
@@ -1,5 +1,14 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "author-1", text: "text-1" },
+  { id: "q2", author: "author-2", text: "text-2" },
+  { id: "q3", author: "author-3", text: "text-3" },
+  { id: "q4", author: "author-4", text: "text-4" },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
