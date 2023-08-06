d10dc906ca9a3e992dac279ccb11293bcabaa7a0
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-08-06 12:38:06 +0330

    Outputting Data on the _Details_ Page

--- Building-Multi-Page-SPA-with-React-Router/Time-to-Practice/src/pages/QuoteDetail.js
+++ Building-Multi-Page-SPA-with-React-Router/Time-to-Practice/src/pages/QuoteDetail.js
@@ -1,15 +1,28 @@
 import { Fragment } from "react";
 import { useParams, Route } from "react-router-dom";
 
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "author-1", text: "text-1" },
+  { id: "q2", author: "author-2", text: "text-2" },
+  { id: "q3", author: "author-3", text: "text-3" },
+  { id: "q4", author: "author-4", text: "text-4" },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
   return (
     <Fragment>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
