9517bd9ab9551e9f70c912ed079bd49b263d5d9c
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-08-18 00:49:02 +0330

    Adding the New Meetup Form

--- Introduction-to-Next.js/Time-to-Practice/pages/new-meetup/index.js
+++ Introduction-to-Next.js/Time-to-Practice/pages/new-meetup/index.js
@@ -1,7 +1,13 @@
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
+function NewMeetupPage() {
+  function addMeetupHandler(enteredMeetupData) {
+    console.log(enteredMeetupData);
+  }
 
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+}
 
-
-
+export default NewMeetupPage;
 
 // our-domain.com/new-meetup
