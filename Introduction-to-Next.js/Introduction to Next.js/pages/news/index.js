03d5e80b0af5407ccda38dfc20f9db3277b0a0e2
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-08-17 23:24:13 +0330

    Linking Between Pages

--- Introduction-to-Next.js/Introduction to Next.js/pages/news/index.js
+++ Introduction-to-Next.js/Introduction to Next.js/pages/news/index.js
@@ -1,5 +1,20 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
 function Newspage() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs">NextJS is a great Freamework</Link>
+        </li>
+        <li>
+          <Link href="/news/reactjs">ReactJS is a great Library</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default Newspage;
