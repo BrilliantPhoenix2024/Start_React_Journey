ad092b0993d88d0cc1310b9efa9fb2de83c4caf9
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-08-19 22:11:32 +0330

    Working with MongoDB

--- Introduction-to-Next.js/Time-to-Practice/pages/api/new-meetup.js
+++ Introduction-to-Next.js/Time-to-Practice/pages/api/new-meetup.js
@@ -1,11 +1,25 @@
-// /api/new-meetup
+import { MongoClient } from "mongodb";
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      "mongodb+srv://<username>:<password>@cluster0.ntrwp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne({ data });
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
 
 export default handler;
+
+// /api/new-meetup
+// POST / api/new-meetup
