5fe040c5bdcab3f91f3cd904bd4e729f5d22e2ef
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-08-19 21:35:36 +0330

    Working with Params for SSG Data Fetching

--- Introduction-to-Next.js/Time-to-Practice/pages/[meetupId]/index.js
+++ Introduction-to-Next.js/Time-to-Practice/pages/[meetupId]/index.js
@@ -12,4 +12,23 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCA53PPrWa1xnn18EV4TWzEenojLDGL7PHXQ&usqp=CAU",
+        id: meetupId,
+        title: "First Meetup",
+        address: "Some Street 5, Some City",
+        description: "This is the frist meetup",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
