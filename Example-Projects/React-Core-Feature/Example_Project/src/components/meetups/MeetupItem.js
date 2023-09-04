a8238a0bee112e1d7f427158377880c2f624d9a2
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-09-04 13:15:03 +0330

    Building Wrapper Components with props.children

--- Example-Projects/React-Core-Feature/Example_Project/src/components/meetups/MeetupItem.js
+++ Example-Projects/React-Core-Feature/Example_Project/src/components/meetups/MeetupItem.js
@@ -1,19 +1,22 @@
+import Card from "../ui/Card";
 import classes from "./MeetupItem.module.css";
 
 function MeetupItem(props) {
   return (
     <li classes={classes.item}>
-      <div classes={classes.image}>
-        <img src={props.image} alt={props.title} />
-      </div>
-      <div classes={classes.content}>
-        <h3>{props.title}</h3>
-        <address>{props.address}</address>
-        <p>{props.description}</p>
-      </div>
-      <div classes={classes.actions}>
-        <button>Add to Favorites</button>
-      </div>
+      <Card>
+        <div classes={classes.image}>
+          <img src={props.image} alt={props.title} />
+        </div>
+        <div classes={classes.content}>
+          <h3>{props.title}</h3>
+          <address>{props.address}</address>
+          <p>{props.description}</p>
+        </div>
+        <div classes={classes.actions}>
+          <button>Add to Favorites</button>
+        </div>
+      </Card>
     </li>
   );
 }
