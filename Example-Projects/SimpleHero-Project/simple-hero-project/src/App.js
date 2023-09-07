d414532d606bab2c3e917edcfc132f361fe22ab9
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-09-07 13:34:12 +0330

    Creating Inputs

--- Example-Projects/SimpleHero-Project/simple-hero-project/src/App.js
+++ Example-Projects/SimpleHero-Project/simple-hero-project/src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">HELLO</div>;
+  return (
+    <div className="App">
+      <h1>Build a Simple Hero</h1>
+      <label>Name: </label>
+      <input type="text" />
+      <label>Age: </label>
+      <input type="number" />
+      <label>Height: </label>
+      <input type="text" />
+      <label>SuperPower: </label>
+      <input type="text" />
+    </div>
+  );
 }
 
 export default App;
