f49a90caedbad1acb112e17d48dfbf63633da2ba
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-09-10 20:44:33 +0330

    Fetching The API Data

--- Example-Projects/SpaceNews-Project/space-news-project/src/App.js
+++ Example-Projects/SpaceNews-Project/space-news-project/src/App.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "./App.css";
 
 function App() {
-  return <div className="App">Space News</div>;
+  useEffect(() => {
+    fetch("https://spaceflightnewsapi.net/api/v2/articles").then((response) =>
+      response.json().then((data) => {
+        console.log(data);
+      })
+    );
+  });
+
+  return (
+    <div className="App">
+      <div className="title">
+        <h1>Space News</h1>
+      </div>
+      <div className="newsContainer"></div>
+    </div>
+  );
 }
 
 export default App;
