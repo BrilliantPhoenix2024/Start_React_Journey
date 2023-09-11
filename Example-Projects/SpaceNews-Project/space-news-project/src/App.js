f7cdf51a41e1a6406da3cbcf8e32120e00948ec5
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-09-11 12:57:33 +0330

    Displaying the API Data

--- Example-Projects/SpaceNews-Project/space-news-project/src/App.js
+++ Example-Projects/SpaceNews-Project/space-news-project/src/App.js
@@ -1,11 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [newsList, setNewsList] = useState([]);
+
   useEffect(() => {
     fetch("https://spaceflightnewsapi.net/api/v2/articles").then((response) =>
       response.json().then((data) => {
-        console.log(data);
+        setNewsList(data);
       })
     );
   });
@@ -15,7 +17,11 @@ function App() {
       <div className="title">
         <h1>Space News</h1>
       </div>
-      <div className="newsContainer"></div>
+      <div className="newsContainer">
+        {newsList.map((val, key) => {
+          return <div key={key}>{val.title}</div>;
+        })}
+      </div>
     </div>
   );
 }
