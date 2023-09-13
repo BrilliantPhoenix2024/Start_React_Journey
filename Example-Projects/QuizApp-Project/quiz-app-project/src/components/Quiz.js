0d81bea86433b64a16f3d52d1cc4f9fa3f6500ae
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-09-13 21:43:45 +0330

    Choosing An Option

--- Example-Projects/QuizApp-Project/quiz-app-project/src/components/Quiz.js
+++ Example-Projects/QuizApp-Project/quiz-app-project/src/components/Quiz.js
@@ -1,8 +1,50 @@
+import { useState } from "react";
 import { Questions } from "../helpers/Questions";
 import "../App.css";
 
 function Quiz() {
-  return <div className="Quiz">{Questions[0].prompt}</div>;
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [optionChosen, setOptionChosen] = useState("");
+
+  const chooseOption = (option) => {
+    setOptionChosen(option);
+  };
+
+  return (
+    <div className="Quiz">
+      <h1>{Questions[currentQuestion].prompt}</h1>
+      <div className="questions">
+        <button
+          onClick={() => {
+            chooseOption("optionA");
+          }}
+        >
+          {Questions[currentQuestion].optionA}
+        </button>
+        <button
+          onClick={() => {
+            chooseOption("optionB");
+          }}
+        >
+          {Questions[currentQuestion].optionB}
+        </button>
+        <button
+          onClick={() => {
+            chooseOption("optionC");
+          }}
+        >
+          {Questions[currentQuestion].optionC}
+        </button>
+        <button
+          onClick={() => {
+            chooseOption("optionD");
+          }}
+        >
+          {Questions[currentQuestion].optionD}
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Quiz;
