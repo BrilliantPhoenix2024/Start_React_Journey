cde3d4f72d19dad05d38a8782e641e9dd9d8af9f
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-09-14 16:15:02 +0330

    Score

--- Example-Projects/QuizApp-Project/quiz-app-project/src/App.js
+++ Example-Projects/QuizApp-Project/quiz-app-project/src/App.js
@@ -8,12 +8,20 @@ import "./App.css";
 function App() {
   const [gameState, setGameState] = useState("menu");
   const [userName, setUserName] = useState("");
+  const [score, setScore] = useState(0);
 
   return (
     <div className="App">
       <h1>Quiz App</h1>
       <GameStateContext.Provider
-        value={{ gameState, setGameState, userName, setUserName }}
+        value={{
+          gameState,
+          setGameState,
+          userName,
+          setUserName,
+          score,
+          setScore,
+        }}
       >
         {gameState === "menu" && <Menu />}
         {gameState === "playing" && <Quiz />}
--- Example-Projects/QuizApp-Project/quiz-app-project/src/components/Quiz.js
+++ Example-Projects/QuizApp-Project/quiz-app-project/src/components/Quiz.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { GameStateContext } from "../helpers/Contexts";
 import { Questions } from "../helpers/Questions";
 import "../App.css";
 
@@ -6,17 +7,16 @@ function Quiz() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [optionChosen, setOptionChosen] = useState("");
 
+  const { score, setScore } = useContext(GameStateContext);
+
   const chooseOption = (option) => {
     setOptionChosen(option);
   };
 
   const nextQuestion = () => {
     if (Questions[currentQuestion].answer == optionChosen) {
-      alert("You got the correct answer!");
-    } else {
-      alert("Wrong answer.");
+      setScore(score + 1);
     }
-
     setCurrentQuestion(currentQuestion + 1);
   };
 
