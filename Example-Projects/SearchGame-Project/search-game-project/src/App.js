7748ef7398ea3d83f0d9f7a027b668a5cee2f4fd
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-09-15 19:07:43 +0330

    Setting up the UI

--- Example-Projects/SearchGame-Project/search-game-project/src/App.js
+++ Example-Projects/SearchGame-Project/search-game-project/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import "./App.css";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <section className="search-section">
+        <h1>Search for a Game</h1>
+        <input type="text" placeholder="Minecraft..." />
+        <button>Search Game Title</button>
+      </section>
+      <section className="deals-section">
+        <h1>Latest Deals</h1>
+      </section>
+    </div>
+  );
 }
 
 export default App;
