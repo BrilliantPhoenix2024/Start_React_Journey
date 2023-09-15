d6b3e616c06a0a405970249cddcd501daa257c36
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-09-15 19:42:45 +0330

    Searching Games

--- Example-Projects/SearchGame-Project/search-game-project/src/App.js
+++ Example-Projects/SearchGame-Project/search-game-project/src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [gameTitle, setGameTitle] = useState("");
+
+  const searchGame = () => {
+    fetch(`https://www.cheapshark.com/api/1.0/games?title=${gameTitle}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
   return (
     <div className="App">
       <section className="search-section">
         <h1>Search for a Game</h1>
-        <input type="text" placeholder="Minecraft..." />
-        <button>Search Game Title</button>
+        <input
+          type="text"
+          placeholder="Minecraft..."
+          onChange={(event) => {
+            setGameTitle(event.target.value);
+          }}
+        />
+        <button onClick={searchGame}>Search Game Title</button>
       </section>
       <section className="deals-section">
         <h1>Latest Deals</h1>
