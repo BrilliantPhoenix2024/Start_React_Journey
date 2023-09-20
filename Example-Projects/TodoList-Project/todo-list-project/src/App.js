df98ad159a0f572e69396e9199909158972e43fd
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-09-20 22:36:22 +0330

    Setting the UI for todo list

--- Example-Projects/TodoList-Project/todo-list-project/src/App.js
+++ Example-Projects/TodoList-Project/todo-list-project/src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+      <div>
+        <input type="text" placeholder="Task..." />
+        <button>Add Task</button>
+      </div>
+      <hr />
+    </div>
+  );
 }
 
 export default App;
