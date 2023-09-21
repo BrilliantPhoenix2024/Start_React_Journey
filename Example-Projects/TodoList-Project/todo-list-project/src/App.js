436b7c4895a9835ecaba4529068a9749c69e51bd
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-09-21 23:35:05 +0330

    Add Tasks to a List

--- Example-Projects/TodoList-Project/todo-list-project/src/App.js
+++ Example-Projects/TodoList-Project/todo-list-project/src/App.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [currentTask, setCurrentTask] = useState("");
+
+  const addTask = () => {
+    setTodoList([...todoList, currentTask]);
+  };
+
   return (
     <div className="App">
       <h1>Todo List</h1>
       <div>
-        <input type="text" placeholder="Task..." />
-        <button>Add Task</button>
+        <input
+          type="text"
+          placeholder="Task..."
+          onChange={(event) => {
+            setCurrentTask(event.target.value);
+          }}
+        />
+        <button onClick={addTask}>Add Task</button>
       </div>
       <hr />
     </div>
