e872fae53ecd12212d74b088199ffb4550f3d750
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-09-24 23:41:40 +0330

    Class Components

--- Example-Projects/Monsters-Project/monsters-project/src/App.js
+++ Example-Projects/Monsters-Project/monsters-project/src/App.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      string: "Hello Atefeh",
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.string}</h1>
+        <button
+          onClick={() => {
+            this.setState({ string: "Hello from React" });
+          }}
+        >
+          Change Text
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
