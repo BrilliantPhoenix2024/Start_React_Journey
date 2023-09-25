6354162a5c5c0ea3d1559d8ad7afef345e621f9f
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-09-26 00:32:01 +0330

    Fetching Content

--- Example-Projects/Monsters-Project/monsters-project/src/App.js
+++ Example-Projects/Monsters-Project/monsters-project/src/App.js
@@ -6,21 +6,14 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: "AAA",
-          id: "A123",
-        },
-        {
-          name: "BBB",
-          id: "B123",
-        },
-        {
-          name: "CCC",
-          id: "C123",
-        },
-      ],
+      monsters: []
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
   render() {
