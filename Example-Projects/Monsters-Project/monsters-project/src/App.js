742892671fe4ced0cefd3451eb8146ecb2fe8135
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-09-27 23:56:55 +0330

    Card List Component

--- Example-Projects/Monsters-Project/monsters-project/src/App.js
+++ Example-Projects/Monsters-Project/monsters-project/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+
+import { CardList } from "./components/card-list.component";
 import "./App.css";
 
 class App extends Component {
@@ -6,7 +8,7 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
     };
   }
 
@@ -19,9 +21,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <CardList>
+          {this.state.monsters.map((monster) => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))}
+        </CardList>
       </div>
     );
   }
