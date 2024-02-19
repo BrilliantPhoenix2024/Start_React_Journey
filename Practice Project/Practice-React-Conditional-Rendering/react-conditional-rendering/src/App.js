a72f504c49ad88a28de7c464c7bf9be6b218ebab
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2024-02-20 00:45:54 +0330

    Conditional Rendering with an external Componenet

--- Practice Project/Practice-React-Conditional-Rendering/react-conditional-rendering/src/App.js
+++ Practice Project/Practice-React-Conditional-Rendering/react-conditional-rendering/src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
-import ConditionalRendering3 from "./Components/ConditionalRendering3/ConditionalRendering3";
+import User from "./Components/User/User";
 
 function App() {
+  const users = [
+    { name: "ali", age: 20 },
+    { name: "atefeh", age: 35 },
+    { name: "nazanin", age: 25 },
+    { name: "mohammad", age: 35 },
+  ];
   return (
     <div className="App">
-      <ConditionalRendering3 />
+      {users.map((user, index)=>{
+        return <User key={index} name={user.name} age={user.age} />;
+      })}
+     
     </div>
   );
 }
