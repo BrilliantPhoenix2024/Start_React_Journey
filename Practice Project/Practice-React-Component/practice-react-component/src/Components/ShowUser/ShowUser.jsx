349a2f4757b396e65ec357e4acf5d7ba2967f4b1
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2024-02-21 01:08:10 +0330

    Improve Code

--- Practice Project/Practice-React-Component/practice-react-component/src/Components/ShowUser/ShowUser.jsx
+++ Practice Project/Practice-React-Component/practice-react-component/src/Components/ShowUser/ShowUser.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const ShowUser = (props) => {
   return (
     <div>
-      <h1>{props.name}</h1>;<h2>{props.age}</h2>;<h3>{props.email}</h3>;
+      <h1>{props.name}</h1>
+      <h2>{props.age}</h2>
+      <h3>{props.email}</h3>
     </div>
   );
 };
