b84a55fbb84977c947ab27e43607ef4cbee0d6b4
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2024-03-13 20:16:58 +0330

    Use {useNavigate()} for navigating between two diffrent page

--- Practice Project/Practice-React-Routes/practice-react-routes/src/Pages/About.jsx
+++ Practice Project/Practice-React-Routes/practice-react-routes/src/Pages/About.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const About = () => {
+  let navigate = useNavigate();
   return (
     <div>
       <h1>About</h1>
-      <button>Go to Home Page</button>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Go to Home Page
+      </button>
     </div>
   );
 };
