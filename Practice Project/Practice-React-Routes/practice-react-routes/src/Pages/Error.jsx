122abb31a0f93c1ca1c2d359b216d5c8a916fd0e
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2024-03-14 20:24:17 +0330

    Navigation with Buttons

--- Practice Project/Practice-React-Routes/practice-react-routes/src/Pages/Error.jsx
+++ Practice Project/Practice-React-Routes/practice-react-routes/src/Pages/Error.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Error = () => {
+  let navigate = useNavigate();
   return (
     <div>
       <h1>Error</h1>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Go to Home Page
+      </button>
     </div>
   );
 };
