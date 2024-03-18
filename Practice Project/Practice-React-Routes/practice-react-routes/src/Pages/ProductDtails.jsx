311d78359ccabef07484cb51495c139c55951cb6
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2024-03-18 12:54:53 +0330

    Create Product Details Page

--- Practice Project/Practice-React-Routes/practice-react-routes/src/Pages/ProductDtails.jsx
+++ Practice Project/Practice-React-Routes/practice-react-routes/src/Pages/ProductDtails.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const ProductDtails = () => {
-  return <div>ProductDtails</div>;
+  let { productId } = useParams();
+  return (
+    <div>
+      <h1>ProductDtails</h1>
+      <h2>This is Details of Product {productId}</h2>
+    </div>
+  );
 };
 
 export default ProductDtails;
