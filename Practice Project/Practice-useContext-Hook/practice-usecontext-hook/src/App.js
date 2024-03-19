be70e0abaeaa4930e6907fe8e385810dfabc7935
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2024-03-20 00:51:47 +0330

    Create Routing

--- Practice Project/Practice-useContext-Hook/practice-usecontext-hook/src/App.js
+++ Practice Project/Practice-useContext-Hook/practice-usecontext-hook/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
+import Contact from "./Pages/Contact";
+import Error from "./Pages/Error";
+import { useState } from "react";
+import { ProfileContext } from "./Context/ProfileContext";
 
 function App() {
+  const [username, setUsername] = useState("Atefeh");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProfileContext.Provider value={{ username, setUsername }}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </BrowserRouter>
+      </ProfileContext.Provider>
     </div>
   );
 }
