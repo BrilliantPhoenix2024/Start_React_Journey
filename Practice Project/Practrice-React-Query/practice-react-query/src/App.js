c1c877aac31e4018d489ed87e2f66c7066ae9876
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2024-04-01 00:18:16 +0330

    Create Routing between Pages

--- Practice Project/Practrice-React-Query/practice-react-query/src/App.js
+++ Practice Project/Practrice-React-Query/practice-react-query/src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from "./Components/Navigation/Nav";
+import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
+import Contact from "./Pages/Contact";
+import Error from "./Pages/Error";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
-  return <div className="App"></div>;
+  const client = new QueryClient();
+  return (
+    <div className="App">
+      <QueryClientProvider client={client}>
+        <BrowserRouter>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </div>
+  );
 }
 
 export default App;
