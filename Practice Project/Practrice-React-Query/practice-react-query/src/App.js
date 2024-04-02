932df91ff5814c9456b1fab73512b1ef28fbea8d
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2024-04-03 01:48:46 +0330

    Add QueryClient and QueryClientProvider

--- Practice Project/Practrice-React-Query/practice-react-query/src/App.js
+++ Practice Project/Practrice-React-Query/practice-react-query/src/App.js
@@ -8,7 +8,11 @@ import Error from "./Pages/Error";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
-  const client = new QueryClient();
+  const client = new QueryClient({
+    defaultOptions: {
+      queries: { refetchOnWindowFocus: false },
+    },
+  });
   return (
     <div className="App">
       <QueryClientProvider client={client}>
