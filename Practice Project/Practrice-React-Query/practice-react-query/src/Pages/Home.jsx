0627939ded24ffdbcf167d9c2191389186cb493f
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2024-04-04 01:49:42 +0330

    Add useQuery() for fetching Data from API

--- Practice Project/Practrice-React-Query/practice-react-query/src/Pages/Home.jsx
+++ Practice Project/Practrice-React-Query/practice-react-query/src/Pages/Home.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const Home = () => {
+  const {
+    data: catData,
+    isLoading,
+    isError,
+    error,
+    refetch,
+  } = useQuery({
+    queryKey: ["cat"],
+    queryFn: async () => {
+      return axios.get("https://catfact.ninja/fact").then((res) => res.data);
+    },
+  });
+
+  if (isLoading) {
+    return <h2>Loading ...</h2>;
+  }
+
+  if (isError) {
+    return <h2>Sorry, something went wrong! error : {error.message}</h2>;
+  }
   return (
     <div>
       <h1>Home</h1>
+      <h1>{catData.fact}</h1>
+      <button onClick={refetch}>Update Fact</button>
     </div>
   );
 };
