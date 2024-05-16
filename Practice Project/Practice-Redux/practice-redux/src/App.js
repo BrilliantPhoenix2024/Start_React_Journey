5fbd7c882c16d4a6615c184696f0c16c4f424fd1
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2024-05-17 02:14:35 +0330

    Navigate Pages in the App

--- Practice Project/Practice-Redux/practice-redux/src/App.js
+++ Practice Project/Practice-Redux/practice-redux/src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Pages/Home.jsx";
+import Contact from "./Pages/Contact.jsx";
+import LoginLogOut from "./Pages/LoginLogOut.jsx";
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={Home} />
+          <Route path="/login" element={LoginLogOut} />
+          <Route path="/contact" element={Contact} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
