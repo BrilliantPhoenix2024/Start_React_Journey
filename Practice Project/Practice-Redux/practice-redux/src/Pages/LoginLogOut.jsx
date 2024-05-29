06ede0f544be39bb85ee23e57fbafbc3046282a6
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2024-05-29 23:06:50 +0330

    import and use useDispatch() and useSelector()

--- Practice Project/Practice-Redux/practice-redux/src/Pages/LoginLogOut.jsx
+++ Practice Project/Practice-Redux/practice-redux/src/Pages/LoginLogOut.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { login, logout } from "../Store/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const LoginLogOut = () => {
-  return <div>LoginLogOut</div>;
+  const [newUsername, setNewUsername] = useState("");
+  const dispatch = useDispatch();
+  const selector = useSelector((state) => state.user.user);
+
+  return (
+    <div>
+      <h1>Login {selector.user}</h1>
+      <input
+        type="name"
+        placeholder="Name"
+        onChange={(e) => setNewUsername(e.target.value)}
+      />
+      <div>
+        <button
+          type="submit"
+          onClick={() => dispatch(login({ user: newUsername }))}
+        >
+          Login
+        </button>
+        <button onClick={() => dispatch(logout())}>logout</button>
+      </div>
+    </div>
+  );
 };
 
 export default LoginLogOut;
