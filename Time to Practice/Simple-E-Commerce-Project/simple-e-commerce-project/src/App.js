4b5863abaf45ea9f78352e31beb61a4008f07bf0
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2024-06-13 17:16:52 +0330

    Implementing Navbar in App

--- Time to Practice/Simple-E-Commerce-Project/simple-e-commerce-project/src/App.js
+++ Time to Practice/Simple-E-Commerce-Project/simple-e-commerce-project/src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Cart from "./Pages/Cart/Cart";
+import NavbarComponent from "./Components/Navbar/Navbar";
+import Shop from "./Pages/Shop/Shop";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <NavbarComponent />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
