6dcc470e34fadb91ecdc1df45b91b3c1960b51d4
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2024-06-21 00:54:25 +0330

    Using shopContext in Product Component

--- Time to Practice/Simple-E-Commerce-Project/simple-e-commerce-project/src/Components/Product/Product.jsx
+++ Time to Practice/Simple-E-Commerce-Project/simple-e-commerce-project/src/Components/Product/Product.jsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shopContext";
 
 const Product = (props) => {
   const { id, image, name, price } = props.data;
+  const { cartItems, addToCart, removeFromCart } = useContext(ShopContext);
+
+  const isInCart = cartItems?.some((item) => item.id === id);
+
   return (
     <div className="col-3">
       <img src={image} />
       <h5> {name}</h5>
       <p>price : ${price}</p>
-      <button className="btn btn-info btn-sm my-5">Add</button>
-      <span className="mx-3">0</span>
-      <button className="btn btn-info btn-sm my-5">Remove</button>
+      <button
+        className="btn btn-info btn-sm my-5"
+        onClick={() => addToCart(id)}
+      >
+        Add
+      </button>
+      <span className="mx-3">
+        {cartItems?.filter((row) => row.id === id)[0]?.count}
+      </span>
+      {isInCart && (
+        <button
+          className="btn btn-info btn-sm my-5"
+          onClick={() => removeFromCart(id)}
+        >
+          Remove
+        </button>
+      )}
     </div>
   );
 };
