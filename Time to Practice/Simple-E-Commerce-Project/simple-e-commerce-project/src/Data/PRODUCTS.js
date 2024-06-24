a12b8c16c40aaddeee72b60b55e7df9fdf034776
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2024-06-24 16:53:21 +0330

    Show Data in Shop Page

--- Time to Practice/Simple-E-Commerce-Project/simple-e-commerce-project/src/Data/PRODUCTS.js
+++ Time to Practice/Simple-E-Commerce-Project/simple-e-commerce-project/src/Data/PRODUCTS.js
@@ -35,7 +35,7 @@ import p34_img from "../Assets/product_34.png";
 import p35_img from "../Assets/product_35.png";
 import p36_img from "../Assets/product_36.png";
 
-export const all_products = [
+export const PRODUCTS = [
   {
     id: 1,
     name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
--- Time to Practice/Simple-E-Commerce-Project/simple-e-commerce-project/src/Pages/Shop/Shop.jsx
+++ Time to Practice/Simple-E-Commerce-Project/simple-e-commerce-project/src/Pages/Shop/Shop.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { all_products } from "../../Data/all_products";
+import { PRODUCTS } from "../../Data/PRODUCTS";
 import Product from "../../Components/Product/Product";
 const Shop = () => {
   return (
     <React.Fragment>
       <h1>Shop</h1>
       <div className="row">
-        {all_products.map((productData) => (
+        {PRODUCTS.map((productData) => (
           <Product key={productData.id} data={productData} />
         ))}
       </div>
