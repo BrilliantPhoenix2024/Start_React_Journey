76c4d8244f26465525738d5ee777d6a95fd64eee
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2024-06-28 18:31:14 +0330

    Add ShopContextCustomProvider to the root App

--- Time to Practice/Simple-E-Commerce-Project/simple-e-commerce-project/src/index.js
+++ Time to Practice/Simple-E-Commerce-Project/simple-e-commerce-project/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { ShopContextProvider } from "./context/shopContext";
+import { ShopContextCustomProvider } from "./context/shopContextCustom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ShopContextProvider>
+    <ShopContextCustomProvider>
       <App />
-    </ShopContextProvider>
+    </ShopContextCustomProvider>
   </React.StrictMode>
 );
 
