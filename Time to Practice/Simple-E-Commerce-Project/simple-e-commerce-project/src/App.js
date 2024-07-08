323b71425baee22a628deefb8c290f6693dfcadb
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2024-07-09 01:26:02 +0330

    Add Wellcome Component to the App

--- Time to Practice/Simple-E-Commerce-Project/simple-e-commerce-project/src/App.js
+++ Time to Practice/Simple-E-Commerce-Project/simple-e-commerce-project/src/App.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Cart from "./Pages/Cart/Cart";
 import NavbarComponent from "./Components/Navbar/Navbar";
 import Shop from "./Pages/Shop/Shop";
+import WellcomeComponent from "./Components/Wellcome/Wellcome";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <NavbarComponent />
+        <WellcomeComponent />
         <Routes>
           <Route path="/" element={<Shop />} />
           <Route path="/cart" element={<Cart />} />
