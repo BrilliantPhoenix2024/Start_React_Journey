32f16f95dfd043ae26a8a51293fb02d8a07a7a5a
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2024-08-19 19:39:49 +0330

    Add reusable LikeComponent Component to the projct

---
+++ Example-Projects/GetMovies/get-movies/src/Components/common/LikeComponent.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+const LikeComponent = (props) => {
+  // Initialize the state to track whether the item is liked
+  const [liked, setLiked] = useState(false);
+
+  // Toggle the liked state when clicked
+  const handleLikeClick = () => {
+    setLiked(!liked);
+  };
+
+  // Determine the class name based on the liked state
+  const clases = liked ? "fa fa-heart" : "fa fa-heart-o";
+
+  return (
+    <React.Fragment>
+      <i className={clases} aria-hidden="true" onClick={handleLikeClick}></i>
+    </React.Fragment>
+  );
+};
+
+export default LikeComponent;
