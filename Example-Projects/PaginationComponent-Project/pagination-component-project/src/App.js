79a7bb7266a21f81c107872d8ab6e9dde175c995
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2024-08-27 21:54:33 +0330

    Use paginate logic in the Movies Component

--- Example-Projects/PaginationComponent-Project/pagination-component-project/src/App.js
+++ Example-Projects/PaginationComponent-Project/pagination-component-project/src/App.js
@@ -1,10 +1,25 @@
+import React, { useState } from "react";
 import "./App.css";
 import Pagination from "./Components/common/Pagination";
 
 function App() {
+  // Define state variables
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsCount = 100; // Total number of items (example)
+  const pageSize = 10; // Number of items per page (example)
+
+  const handleOnPageChange = (page) => {
+    setCurrentPage(page); // Update the current page
+  };
+
   return (
     <div className="App">
-      <Pagination />
+      <Pagination
+        itemsCount={itemsCount}
+        pageSize={pageSize}
+        onPageChange={handleOnPageChange}
+        currentPage={currentPage}
+      />
     </div>
   );
 }
