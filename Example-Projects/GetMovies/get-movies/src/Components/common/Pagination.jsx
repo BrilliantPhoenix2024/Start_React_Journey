adcae8d69814584333fb560b4c47dc6bb65084e3
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2024-08-28 21:57:47 +0330

    Create Pagination Component

---
+++ Example-Projects/GetMovies/get-movies/src/Components/common/Pagination.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import _ from "lodash";
+
+const Pagination = (props) => {
+  const { itemsCount, pageSize, onPageChange, currentPage } = props;
+
+  const pagesCount = Math.ceil(itemsCount / pageSize);
+  if (pagesCount === 1) return null; // No pagination needed if only one page
+  const pages = _.range(1, pagesCount + 1); // Create an array of page numbers
+
+  return (
+    <React.Fragment>
+      <ul className="pagination">
+        {pages.map((page) => (
+          <li
+            key={page}
+            className={page === currentPage ? "page-item active" : "page-item"}
+          >
+            <a className="page-link" onClick={() => onPageChange(page)}>
+              {page}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </React.Fragment>
+  );
+};
+
+export default Pagination;
