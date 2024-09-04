9083a648215c514f4a2340ff0ec4e63c16a514af
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2024-09-04 23:39:24 +0330

    Create ListGroup Component

---
+++ Example-Projects/GetMovies/get-movies/src/Components/common/ListGroup.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ListGroup = ({ items, selectedItem, onItemSelect }) => {
+  return (
+    <ul className="list-group">
+      {items.map((item) => (
+        <li
+          key={item._id}
+          className={
+            item === selectedItem ? "list-group-item active" : "list-group-item"
+          }
+          onClick={() => onItemSelect(item)}
+          style={{ cursor: "pointer" }}
+        >
+          {item.name}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ListGroup;
