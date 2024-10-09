5c7ab9e5358be8d77445d35076b73fef21b39483
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2024-10-09 19:00:48 +0330

    Create Routing for diffrent pages in the App

--- Example-Projects/GetMovies/get-movies/src/App.js
+++ Example-Projects/GetMovies/get-movies/src/App.js
@@ -1,12 +1,23 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavbarComponent from "./Components/NavbarComponent.jsx";
+import About from "./pages/About.jsx"; 
+import Contact from "./pages/Contact"; 
+import LinkPage from "./pages/LinkPage"; 
 import Movies from "./Components/Movies";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Movies />
-    </div>
+    <Router>
+      <NavbarComponent />
+      <Routes>
+        <Route path="/" element={<Movies />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/link" element={<LinkPage />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
