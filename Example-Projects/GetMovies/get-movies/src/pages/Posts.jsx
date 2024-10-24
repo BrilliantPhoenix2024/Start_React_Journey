d245dba9b0ae043d6720e74470585cc5d17c8da5
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2024-10-24 19:00:31 +0330

    Add nested Routing In Posts Page

--- Example-Projects/GetMovies/get-movies/src/pages/Posts.jsx
+++ Example-Projects/GetMovies/get-movies/src/pages/Posts.jsx
@@ -1,14 +1,24 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Routes, Route, useParams, Link } from "react-router-dom";
+import SideBarComponent from "../Components/SideBarComponent";
+import AdminContent from "../Components/Admin";
+import UserContent from "../Components/Users";
 
 const Posts = () => {
-  const { year, month } = useParams();
+  // const { year, month } = useParams();
+
   return (
     <React.Fragment>
       <h1>Posts</h1>
-      <p>
+      {/* <p>
         Year: {year} , Month: {month}
-      </p>
+      </p> */}
+      <SideBarComponent />
+
+      <Routes>
+        <Route path="admin" element={<AdminContent />} />
+        <Route path="users" element={<UserContent />} />
+      </Routes>
     </React.Fragment>
   );
 };
