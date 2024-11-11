5c97213e199bbc32aeeb4877bd939d54c4f75c8a
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2024-11-11 18:52:03 +0330

    Basic Validation Implementation

--- Example-Projects/GetMovies/get-movies/src/Components/LoginForm.jsx
+++ Example-Projects/GetMovies/get-movies/src/Components/LoginForm.jsx
@@ -3,6 +3,7 @@ import Input from "./common/Input";
 
 const LoginForm = () => {
   const [account, setAccount] = useState({ username: "", password: "" });
+  const [errors, setErrors] = useState({});
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -12,8 +13,21 @@ const LoginForm = () => {
     }));
   };
 
+  const validate = () => {
+    const errors = {};
+    if (!account.username) errors.username = "Username is required.";
+    if (!account.password) errors.password = "Password is required.";
+    return errors;
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    const validationErrors = validate();
+    setErrors(validationErrors);
+
+    if (Object.keys(validationErrors).length > 0) return; // Stop submission if there are errors
+
+    // If no errors, proceed with submission logic
     console.log("Submitted");
     console.log("Username:", account.username);
     console.log("Password:", account.password);
@@ -29,6 +43,7 @@ const LoginForm = () => {
           name="username"
           value={account.username}
           onChange={handleChange}
+          error={errors.username} // Pass error message to Input component
         />
 
         <Input
@@ -37,6 +52,7 @@ const LoginForm = () => {
           name="password"
           value={account.password}
           onChange={handleChange}
+          error={errors.password} // Pass error message to Input component
         />
 
         <button type="submit" className="btn btn-primary">
--- Example-Projects/GetMovies/get-movies/src/Components/common/Input.jsx
+++ Example-Projects/GetMovies/get-movies/src/Components/common/Input.jsx
@@ -1,18 +1,9 @@
-// Input.js
-import React from 'react';
-
-const Input = ({ label, type, name, value, onChange }) => {
+const Input = ({ label, error, ...rest }) => {
   return (
     <div className="form-group">
-      <label htmlFor={name}>{label}</label>
-      <input
-        id={name}
-        name={name}
-        type={type}
-        className="form-control"
-        value={value}
-        onChange={onChange}
-      />
+      <label>{label}</label>
+      <input className="form-control" {...rest} />
+      {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
 };
