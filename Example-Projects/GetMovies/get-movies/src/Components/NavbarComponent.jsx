f634255e25ffff167367bbdb24778b58bb2f0f08
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2024-11-22 23:28:21 +0330

    Add Register Tab in the NavbarComponent

--- Example-Projects/GetMovies/get-movies/src/Components/NavbarComponent.jsx
+++ Example-Projects/GetMovies/get-movies/src/Components/NavbarComponent.jsx
@@ -24,6 +24,9 @@ const NavbarComponent = () => {
             <Nav.Link as={Link} to="/login">
               Login
             </Nav.Link>
+            <Nav.Link as={Link} to="/register">
+              Register
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
