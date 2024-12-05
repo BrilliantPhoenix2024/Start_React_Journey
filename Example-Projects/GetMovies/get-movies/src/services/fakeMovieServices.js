daaa63f30ec6dfbf7b1c0a43f194182db0a5e6c0
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2024-12-05 17:58:16 +0330

    Modify some functions in fakeMovieServise

--- Example-Projects/GetMovies/get-movies/src/services/fakeMovieServices.js
+++ Example-Projects/GetMovies/get-movies/src/services/fakeMovieServices.js
@@ -1,5 +1,5 @@
 import * as genresAPI from "./fakeGenreService";
-// import randomstring from "random-string";
+import randomstring from "random-string";
 export const movies = [
   {
     _id: "5b21ca3eeb7f6fbccd471815",
@@ -67,34 +67,36 @@ export const movies = [
   },
 ];
 
+const allGenres = genresAPI.getGenres(); // This calls the getGenres function
+
 export function getMovies() {
   return movies;
 }
 
-// export function getMovie(id) {
-//   return movies.find((m) => m._id === id);
-// }
+export function getMovie(id) {
+  return movies.find((m) => m._id === id);
+}
 
-// export function saveMovie(movie) {
-//   let movieInDb = movies.find((m) => m._id === movie._id) || {};
-//   movieInDb.title = movie.title;
-//   movieInDb.genre = genresAPI.genres.find((g) => g._id === movie.genreId);
-//   movieInDb.numberInStock = movie.numberInStock;
-//   movieInDb.dailyRentalRate = movie.dailyRentalRate;
+export function saveMovie(movie) {
+  let movieInDb = movies.find((m) => m._id === movie._id) || {};
+  movieInDb.title = movie.title;
+  movieInDb.genre = allGenres.genres.find((g) => g._id === movie.genreId);
+  movieInDb.numberInStock = movie.numberInStock;
+  movieInDb.dailyRentalRate = movie.dailyRentalRate;
 
-//   if (!movieInDb._id) {
-//     movieInDb._id = randomstring({
-//       length: 15,
-//       numeric: true,
-//       letters: true,
-//       special: false,
-//       exclude: ["a", "b", "1"],
-//     });
-//     movies.push(movieInDb);
-//   }
+  if (!movieInDb._id) {
+    movieInDb._id = randomstring({
+      length: 15,
+      numeric: true,
+      letters: true,
+      special: false,
+      exclude: ["a", "b", "1"],
+    });
+    movies.push(movieInDb);
+  }
 
-//   return movieInDb;
-// }
+  return movieInDb;
+}
 
 // export function deleteMovie(id) {
 //   let movieInDb = movies.find((m) => m._id === id);
