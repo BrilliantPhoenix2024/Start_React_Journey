c14d2da70d7bc54974f30ca249bd44df163874c5
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2024-12-09 00:32:26 +0330

    Create SearchBox Component

---
+++ Example-Projects/GetMovies/get-movies/src/Components/common/SearchBox.jsx
@@ -0,0 +1,16 @@
+// SearchBox.jsx
+import React from "react";
+
+const SearchBox = ({ value, onChange }) => {
+  return (
+    <input
+      type="text"
+      placeholder="Search..."
+      value={value}
+      onChange={(e) => onChange(e.currentTarget.value)}
+      className="form-control my-3"
+    />
+  );
+};
+
+export default SearchBox;
