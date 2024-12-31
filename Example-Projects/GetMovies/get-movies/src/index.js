b04fa8b90bb74e2b578193b6924ec23c77267f6a
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2024-12-31 20:33:15 +0330

    Add sentry/react in the root

--- Example-Projects/GetMovies/get-movies/src/index.js
+++ Example-Projects/GetMovies/get-movies/src/index.js
@@ -1,20 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import * as Sentry from "@sentry/react";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+Sentry.init({
+  dsn: "https://61c1f1dedbb1d98fd618b0fc3c3866b3@o4509916611739648.ingest.de.sentry.io/4509916723544144",
+  sendDefaultPii: true,
+});
+
+const container = document.getElementById("root");
+const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
