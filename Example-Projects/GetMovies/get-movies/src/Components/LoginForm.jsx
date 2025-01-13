98d760c2fe244aa9974e1389c8b598ad064bb550
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2025-01-13 18:20:56 +0330

    Getting the Current User

--- Example-Projects/GetMovies/get-movies/src/Components/LoginForm.jsx
+++ Example-Projects/GetMovies/get-movies/src/Components/LoginForm.jsx
@@ -1,21 +1,47 @@
 import React from "react";
-import Joi from "joi-browser"; // Import Joi
-import Form from "./common/Form"; // Import the base Form class
+import Joi from "joi-browser";
+import Form from "./common/Form";
+import { Navigate } from "react-router-dom";
 
 class LoginForm extends Form {
+  state = {
+    data: { username: "", password: "" },
+    errors: {},
+    redirect: false,
+  };
+
   schema = Joi.object({
     username: Joi.string().required().label("Username"),
     password: Joi.string().required().label("Password"),
   });
 
-  doSubmit = () => {
-    // Call the server or perform submission logic
-    console.log("Submitted");
-    console.log("Username:", this.state.data.username);
-    console.log("Password:", this.state.data.password);
+  doSubmit = async () => {
+    const { username, password } = this.state.data;
+
+    try {
+      const response = await fetch("https://your-api.com/api/auth", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (!response.ok) throw new Error("Invalid credentials");
+
+      const data = await response.json();
+      localStorage.setItem("token", data.token); // Save JWT
+      this.setState({ redirect: true }); // Trigger redirect
+    } catch (error) {
+      const errors = { ...this.state.errors };
+      errors.username = "Login failed. Please check your credentials.";
+      this.setState({ errors });
+    }
   };
 
   render() {
+    if (this.state.redirect) return <Navigate to="/" />;
+
     return (
       <div className="w-75 m-auto">
         <h1>Login</h1>
